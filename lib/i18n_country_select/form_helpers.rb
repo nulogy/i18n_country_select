2ac60328f5f75d2757aefa1a2fa4c7a0ed333d45
Alistair McKinnell <alistairm@nulogy.com>
2021-04-11 12:11:37 -0400

    Drop support for old versions of Rails

--- lib/i18n_country_select/form_helpers.rb
+++ lib/i18n_country_select/form_helpers.rb
@@ -1,13 +1,8 @@
 module I18nCountrySelect
   module FormHelpers
     def country_code_select(object_name, method, priority_countries = nil, options = {}, html_options = {})
-      if Rails::VERSION::MAJOR >= 4
-        instance_tag = ActionView::Helpers::Tags::Select.new(object_name, method, self, [], options, html_options)
-        instance_tag.to_country_code_select_tag(priority_countries, html_options)
-      else
-        instance_tag = ActionView::Helpers::InstanceTag.new(object_name, method, self, options.delete(:object))
-        instance_tag.to_country_code_select_tag(priority_countries, html_options, options)
-      end
+      instance_tag = ActionView::Helpers::Tags::Select.new(object_name, method, self, [], options, html_options)
+      instance_tag.to_country_code_select_tag(priority_countries, html_options)
     end
   end
 end
